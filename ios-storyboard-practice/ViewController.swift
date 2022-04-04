9c112802f978c0a2ef15f322ddd650c5a379d686
d.kanai <contact@dkanai.com>
2022-04-04 19:09:46 +0900

    bind button action

--- ios-storyboard-practice/ViewController.swift
+++ ios-storyboard-practice/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tappedButton(_ sender: Any) {
+        print("tapped button")
+    }
+    
 }
 
