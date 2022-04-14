d60393720e6c4569f0afd1f3810c7bee92c3a5b8
d.kanai <contact@dkanai.com>
2022-04-14 12:15:31 +0900

    refactoring

--- ios-storyboard-practiceTests/ios_storyboard_practiceTests.swift
+++ ios-storyboard-practiceTests/ios_storyboard_practiceTests.swift
@@ -9,16 +9,26 @@ import XCTest
 @testable import ios_storyboard_practice
 
 class CounterControllerTests: XCTestCase {
-    func test_画面が表示されたら_ラベルに0が表示される() {
+    
+    var vc: CounterViewController!
+    
+    override func setUp() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "CounterViewController") as? CounterViewController
-        
-        let label = viewController?.view.subviews
-            .filter({ $0.accessibilityIdentifier == "countLabel" })
-            .compactMap({ $0 as? UILabel })
-            .first
-
-        XCTAssertEqual(label?.text, "0")
+        vc = storyboard.instantiateViewController(withIdentifier: "CounterViewController") as? CounterViewController
     }
+
+    func test_show_default_counter_label() {
+        let label = findById(id: "countLabel") as UILabel
+        XCTAssertEqual(label.text, "0")
+    }
+    
+    func findById<T>(id:String) -> T {
+        return vc.view.subviews
+            .filter({ $0.accessibilityIdentifier == id })
+            .compactMap({ $0 as? T })
+            .first!
+        
+    }
+    
 }
 
